68e43fb24b3d574dec065ea9e5f35de19609c13d
Mst.Fahmida Akter <fahmidaakter5nov@gmail.com>
2024-03-02 03:03:44 +0600

    8th commit

--- lib/profile_full_page/profile_list.dart
+++ lib/profile_full_page/profile_list.dart
@@ -5,12 +5,16 @@ class ProfileListItem extends StatelessWidget {
   final IconData icon;
   final String text;
   final bool hasNavigation;
+  final Color iconColor;
+  final Color textColor;
 
-  const ProfileListItem( {
-    super.key,
+  const ProfileListItem({
+    super.key, // removed super.key
     required this.icon,
     required this.text,
     this.hasNavigation = true,
+    required this.iconColor,
+    required this.textColor,
   });
 
   @override
@@ -20,34 +24,35 @@ class ProfileListItem extends StatelessWidget {
       margin: const EdgeInsets.symmetric(
         horizontal: 8,
       ).copyWith(
-        bottom:  20,
+        bottom: 20,
       ),
       padding: const EdgeInsets.symmetric(
-        horizontal:  24.0,
+        horizontal: 24.0,
       ),
       decoration: BoxDecoration(
         color: Colors.grey.shade50,
         borderRadius: BorderRadius.circular(15),
-
       ),
       child: Row(
         children: <Widget>[
           Icon(
             icon,
-            size:  24.0,
+            size: 24.0,
+            color: iconColor,
           ),
-          const SizedBox(width: 6),
+           const SizedBox(width: 6),
           Text(
             text,
-            style: const TextStyle(
+            style: TextStyle(
               fontWeight: FontWeight.w500,
+              color: textColor,
             ),
           ),
           const Spacer(),
           if (hasNavigation)
             const Icon(
               LineAwesomeIcons.angle_right,
-              size:  24.0,
+              size: 24.0,
             ),
         ],
       ),
